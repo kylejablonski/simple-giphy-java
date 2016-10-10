b06461ebdd29c63ded271ba895ba1d850238edd0
Kyle Jablonski <kyle@eventjoy.com>
2016-10-10 13:14:52 -0700

    moved all tests to one folder

--- test/com/kdotj/simplegiphy/SimpleGiphyTest.java
+++ test/com/kdotj/simplegiphy/SimpleGiphyTest.java
@@ -110,4 +110,62 @@ public class SimpleGiphyTest {
 		}
 		assertNotNull(giphyListResponse);
 	}
+	
+
+	@Test
+	public void testRandomSticker(){
+		System.out.println("testRandomSticker() -> american+psycho");
+		System.out.println("");
+		
+		RandomGiphyResponse randomGiphyResponse = mInstance.randomSticker("oops", "pg-13");
+		try {
+			System.out.println(new ObjectMapper().writeValueAsString(randomGiphyResponse.getRandomGiphy()));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(randomGiphyResponse.getRandomGiphy());
+	}
+	
+	@Test
+	public void testTrendingStickers(){
+		System.out.println("testTrendingStickers() -> 1, pg-13");
+		System.out.println("");
+		
+		GiphyListResponse trendingResponse = mInstance.trendingStickers("1", "pg-13");
+		List<Giphy> trendingList = trendingResponse.getData();
+		try {
+			System.out.println(new ObjectMapper().writeValueAsString(trendingResponse));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(trendingList);
+	}
+	
+	@Test
+	public void testSearchStickers(){
+		System.out.println("testSearchSticker() -> cats, 2, 0, pg-13");
+		System.out.println("");
+		GiphyListResponse listResponse = mInstance.searchStickers("cats", "2", "0" , "pg-13");
+		List<Giphy> trendingList = listResponse.getData();
+		try {
+			System.out.println(new ObjectMapper().writeValueAsString(listResponse));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(trendingList);
+	}
+	
+	@Test
+	public void testTranslateSticker(){
+		System.out.println("testTranslateSticker() -> awesome, pg-13");
+		System.out.println("");
+		SimpleGiphyResponse translateResponse = mInstance.translateSticker("awesome", "pg-13");
+		Giphy giphy = translateResponse.getData();
+		try {
+			System.out.println(new ObjectMapper().writeValueAsString(giphy));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(giphy);
+	}
 }
